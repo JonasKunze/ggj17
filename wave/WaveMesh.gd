1a2a9787b7086438696e6d00a193f3d188bb1c70
Jonas Kunze <kunze.jonas@gmail.com>
2017-01-21 00:14:52 +0100

    Surface tool only

--- wave/WaveMesh.gd
+++ wave/WaveMesh.gd
@@ -3,8 +3,8 @@ extends MeshInstance
 #export(FixedMaterial)    var material    = null
 var myMesh = null
 var gameTimePassed = 0
-export var sizeX = 10
-export var sizeZ = 10
+export var sizeX = 2
+export var sizeZ = 2
 export var springConstant = 2
 
 var vertices = []
@@ -20,7 +20,7 @@ func initVertices():
 		vertices.append(col)
 		for x in range(0, sizeX):
 			col.append(Vector3(x, 0, z))
-		
+
 func createMesh():
 	var surfTool = SurfaceTool.new()
 	myMesh = Mesh.new()
@@ -51,25 +51,10 @@ func addVerticesToMesh(surfTool):
 			surfTool.add_vertex(vertices[z+1][x])
 
 func _process(delta):
+	print(1/delta)
 	gameTimePassed += delta
-	var datatool = MeshDataTool.new()
-	datatool.create_from_surface(myMesh, 0)
 	for z in range(0, sizeX):
 		for x in range(0, sizeZ):
 			var vertexId = x*sizeZ + z
-			var vertex = datatool.get_vertex(vertexId)
 			vertices[z][x].y = sin(gameTimePassed*vertexId)
-			datatool.set_vertex(vertexId, vertex)
-			
-	writeVerticesToMesh(datatool)
-	myMesh.surface_remove(0) #Remove the old one
-	datatool.commit_to_surface(myMesh)
-
-	set_mesh(myMesh)
-	
-func writeVerticesToMesh(datatool):
-	print(datatool.get_vertex_count())
-	for z in range(0, sizeZ):
-		for x in range(0, sizeX):
-			var vertexId = z*sizeX + x
-			datatool.set_vertex(vertexId, vertices[z][x])
+	createMesh()
