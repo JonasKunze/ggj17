ad64e2eb8c359712b518819df27a167f0f7d4823
Kjili <Kjili@users.noreply.github.com>
2017-01-21 15:55:16 +0100

    merge and make player jump

--- player/player.gd
+++ player/player.gd
@@ -7,23 +7,39 @@ extends KinematicBody
 export var speed = 10
 export var jumpSpeed = 4
 var velocity = Vector3()
+var keyPressCounter = 0
+
+func _input(event):
+	var snowmanAnimationPlayer = get_node("snowman/AnimationPlayer")
+	if event.is_action_pressed("ui_left") or event.is_action_pressed("ui_right")  or event.is_action_pressed("ui_up")  or event.is_action_pressed("ui_down"):
+		keyPressCounter += 1
+		snowmanAnimationPlayer.play("hop")
+	if event.is_action_released("ui_left") or event.is_action_released("ui_right")  or event.is_action_released("ui_up")  or event.is_action_released("ui_down"):
+		keyPressCounter -= 1
+	if keyPressCounter > 0:
+		snowmanAnimationPlayer.get_animation("hop").set_loop(true)
+	else:
+		snowmanAnimationPlayer.get_animation("hop").set_loop(false)
 
 export var jumpTimeout_s = 2
 var jumped = false
 var lastJumpTime = 0
 
 func _fixed_process(delta):
-	#print(get_transform())
 	if (Input.is_action_pressed("ui_left")):
 		velocity.x = -speed
+		set_rotation(Vector3(0,PI,0))
 	elif (Input.is_action_pressed("ui_right")):
 		velocity.x = speed
+		set_rotation(Vector3(0,2*PI,0))
 	else:
 		velocity.x = 0
 	if (Input.is_action_pressed("ui_up")):
 		velocity.z = -speed
+		set_rotation(Vector3(0,0.5*PI,0))
 	elif (Input.is_action_pressed("ui_down")):
 		velocity.z = speed
+		set_rotation(Vector3(0,1.5*PI,0))
 	else:
 		velocity.z = 0
 	
@@ -51,3 +67,4 @@ func _fixed_process(delta):
 
 func _ready():
 	set_fixed_process(true)
+	set_process_input(true)
