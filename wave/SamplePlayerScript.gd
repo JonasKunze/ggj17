75f2aab5f5c596674730f3f32fb4717b82a8b593
Sebastian Artz <sebastian.artz85@gmail.com>
2017-01-21 13:07:24 +0100

    added sample player

---
+++ wave/SamplePlayerScript.gd
@@ -0,0 +1,11 @@
+extends SpatialSamplePlayer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	playDropSound()
+	
+func playDropSound():
+	play("drop01")
