df94dff09e9a5cafe3ab75108391fc1038dcc68a
Jonas Kunze <kunze.jonas@gmail.com>
2017-01-21 02:00:41 +0100

    First realistic wave simulation

--- wave/WaveMesh.gd
+++ wave/WaveMesh.gd
@@ -5,9 +5,12 @@ var myMesh = null
 var gameTimePassed = 0
 export var sizeX = 2
 export var sizeZ = 2
-export var springConstant = 2
+var springConstant = 40
+var friction = 0.05
 
 var vertices = []
+var velocities = []
+var lastVertices = []
 
 export(Material) var material = null
 
@@ -19,9 +22,19 @@ func _ready():
 func initVertices():
 	for z in range(0, sizeZ):
 		var col = []
+		var lastCol = []
+		var velos = []
 		vertices.append(col)
+		lastVertices.append(lastCol)
+		velocities.append(velos)
+		
 		for x in range(0, sizeX):
 			col.append(Vector3(x, 0, z))
+			lastCol.append(Vector3(x, 0, z))
+			velos.append(0)
+			
+	vertices[50][50] = Vector3(0, 100, 0)
+	lastVertices[50][50] = vertices[50][50]
 
 func createMesh():
 	var surfTool = SurfaceTool.new()
@@ -52,12 +65,21 @@ func addVerticesToMesh(surfTool):
 			surfTool.add_vertex(vertices[z+1][x+1])
 			surfTool.add_vertex(vertices[z+1][x])
 
-func _process(delta):
-	print(1/delta)
-	gameTimePassed += delta
-	for z in range(0, sizeX):
-		for x in range(0, sizeZ):
-			var vertexId = x*sizeZ + z
-			vertices[z][x].y = sin(0.1*gameTimePassed*vertexId)
-	print(vertices[9][9])
+func _process(deltaT):
+	print(1/deltaT)
+	gameTimePassed += deltaT
+	var tsquareHalf = deltaT * deltaT / 2.0
+	for z in range(1, sizeX-1):
+		for x in range(1, sizeZ-1):
+			var currentY = vertices[z][x].y
+			var north = lastVertices[z+1][x].y - currentY
+			var east = lastVertices[z][x+1].y - currentY
+			var south = lastVertices[z-1][x].y - currentY
+			var west = lastVertices[z][x-1].y - currentY
+			var force = springConstant * (north+east+south+west) - friction * velocities[z][x]
+			
+			vertices[z][x].y = tsquareHalf*force + velocities[z][x] * deltaT
+			velocities[z][x] += force*deltaT
+			
+	lastVertices = vertices
 	createMesh()
