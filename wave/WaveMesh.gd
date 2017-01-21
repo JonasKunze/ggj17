498d142906ac0f8b1aadd0423f49785e3addbaf8
Jonas Kunze <kunze.jonas@gmail.com>
2017-01-21 04:56:56 +0100

    Generating boxes instead of meshes

--- wave/WaveMesh.gd
+++ wave/WaveMesh.gd
@@ -1,4 +1,4 @@
-extends MeshInstance
+extends Spatial
 
 #export(FixedMaterial)    var material    = null
 var myMesh = null
@@ -8,82 +8,62 @@ export var sizeZ = 2
 var springConstant = 15
 var friction = 0.01
 
-var vertices = []
+var boxes = []
 var velocities = []
 var lastVertices = []
+
+var mapscene = load("res://groundBox/groundBox.tscn")
 
 export(Material) var material = null
 
 func _ready():
-	initVertices()
-	createMesh()
+	initBoxes()
 	self.set_process(true)
 	
-func initVertices():
+func initBoxes():
 	for z in range(0, sizeZ):
 		var col = []
 		var lastCol = []
 		var velos = []
-		vertices.append(col)
+		boxes.append(col)
 		lastVertices.append(lastCol)
 		velocities.append(velos)
 		
 		for x in range(0, sizeX):
-			col.append(Vector3(x, 0, z))
-			lastCol.append(Vector3(x, 0, z))
+			var pos = Vector3(x-sizeX/2.0, 0, z-sizeZ/2.0)
+			col.append(createBox(pos))
+			lastCol.append(pos)
 			velos.append(0)
 			
-	vertices[50][50] = Vector3(50, 100, 50)
-	lastVertices[50][50] = vertices[50][50]
+	boxes[sizeZ/2.0][sizeX/2.0].set_translation(Vector3(0, 100, 0))
+	lastVertices[sizeZ/2.0][sizeX/2.0] = boxes[sizeZ/2.0][sizeX/2.0].get_translation()
 
-func createMesh():
-	var surfTool = SurfaceTool.new()
-	myMesh = Mesh.new()
-	#var material = FixedMaterial.new()
-	#material.set_parameter(material.PARAM_DIFFUSE,Color(1,0,0,1))
-  
-	surfTool.set_material(material)
-	surfTool.begin(VS.PRIMITIVE_TRIANGLES)
-	
-	addVerticesToMesh(surfTool)
-
-	surfTool.generate_normals()
-	surfTool.index()
-	  
-	surfTool.commit(myMesh)
-	  
-	self.set_mesh(myMesh)
-	
-func addVerticesToMesh(surfTool):
-	for z in range(0, sizeZ-1):
-		for x in range(0, sizeX-1):
-			surfTool.add_vertex(vertices[z][x])
-			surfTool.add_vertex(vertices[z][x+1])
-			surfTool.add_vertex(vertices[z+1][x+1])
-			
-			surfTool.add_vertex(vertices[z][x])
-			surfTool.add_vertex(vertices[z+1][x+1])
-			surfTool.add_vertex(vertices[z+1][x])
+func createBox(position):
+	var box = mapscene.instance()
+	add_child(box)
+	box.set_translation(Vector3(position))
+	print(position)
+	return box
 
 func _process(deltaT):
 	print(1/deltaT)
 	gameTimePassed += deltaT
+	
 	var tsquareHalf = deltaT * deltaT / 2.0
 	for z in range(1, sizeX-1):
 		for x in range(1, sizeZ-1):
-			var currentY = vertices[z][x].y
+			var currentPos = boxes[z][x].get_translation()
 			var force = 0
 			for i in range(-1, 2):
 				for j in range(-1, 2):
 					if abs(i) + abs(j) == 2:
-						force += 0.707* (lastVertices[z+i][x+j].y - currentY)
+						force += 0.707* (lastVertices[z+i][x+j].y - currentPos.y)
 					else:
-						force += lastVertices[z+i][x+j].y - currentY
-			
+						force += lastVertices[z+i][x+j].y - currentPos.y
+
 			force *= springConstant * (1-friction)
-			
-			vertices[z][x].y = tsquareHalf*force + velocities[z][x] * deltaT
+			currentPos.y = tsquareHalf*force + velocities[z][x] * deltaT
+			boxes[z][x].set_translation(currentPos)
 			velocities[z][x] += force*deltaT
+			lastVertices[z][x] = boxes[z][x].get_translation()
 	
-	lastVertices = vertices
-	createMesh()
