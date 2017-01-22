32fbda7729e37f14f85ad2335a6320a8a1005f57
Johannes Mattmann <johannes.mattmann@cern.ch>
2017-01-22 14:33:40 +0100

    Snowman kann sterben, wird nach Countdown eingefroren

--- gui/gui.gd
+++ gui/gui.gd
@@ -1,12 +1,15 @@
 extends Control
 
-var countdown = 60
+export var countdown = 60
+var ball
+var snowmanAnimationPlayer = null
+var finished = false
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	set_process(true)
-	
+	ball = get_node("/root/Spatial/Ball")
 
 func _process(delta):
 	countdown -= delta
@@ -14,3 +17,13 @@ func _process(delta):
 		get_node("time").set_text(str(round(countdown)))
 	else:
 		get_node("time").set_text("Time Over")
+		var looser = null
+		if ball.points1 > ball.points2:
+			looser = get_node("/root/Spatial/Player1")
+		elif ball.points1 < ball.points2:
+			looser = get_node("/root/Spatial/Player2")
+		
+		if looser != null and !snowmanAnimationPlayer:
+			snowmanAnimationPlayer = looser.get_node("snowman_mesh/AnimationPlayer")
+			snowmanAnimationPlayer.play("die")
+		finished = true
